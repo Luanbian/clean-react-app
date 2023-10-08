246301537cd5ba79bb53731342dee47e572204f6
Luan Bianchini <luanbianchine1@gmail.com>
2023-10-08 16:12:36 -0300

    feat: add axios and call with correct url

---
+++ src/infra/http/axios/axios.http.client.spec.ts
@@ -0,0 +1,15 @@
+import { faker } from "@faker-js/faker";
+import axios from "axios";
+import { AxiosHttpCLient } from "./axios.http.client";
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe("AxiosHttpClient", () => {
+  test("should call axios with correct url", async () => {
+    const sut = new AxiosHttpCLient();
+    const url = faker.internet.url();
+    await sut.post({ url });
+    expect(mockedAxios).toHaveBeenCalledWith(url);
+  });
+});
