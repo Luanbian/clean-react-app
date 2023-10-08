c351f7a9b9b6f6b626d88785383c1d727cbb19f2
Luan Bianchini <luanbianchine1@gmail.com>
2023-10-08 16:15:50 -0300

    feat: ensure the method called in url should be post

--- src/infra/http/axios/axios.http.client.spec.ts
+++ src/infra/http/axios/axios.http.client.spec.ts
@@ -5,11 +5,15 @@ import { AxiosHttpCLient } from "./axios.http.client";
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
+const makeSut = (): AxiosHttpCLient => {
+  return new AxiosHttpCLient();
+};
+
 describe("AxiosHttpClient", () => {
-  test("should call axios with correct url", async () => {
-    const sut = new AxiosHttpCLient();
+  test("should call axios with correct url and verb", async () => {
+    const sut = makeSut();
     const url = faker.internet.url();
     await sut.post({ url });
-    expect(mockedAxios).toHaveBeenCalledWith(url);
+    expect(mockedAxios.post).toHaveBeenCalledWith(url);
   });
 });
